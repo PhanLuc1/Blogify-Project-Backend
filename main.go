10d68b5fd48fd45df730e49d90097ddbc31c0099
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-04-25 04:40:16 +0700

    feat: setup server

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/PhanLuc1/Blogify-Project-Backend/src/middleware"
+	"github.com/PhanLuc1/Blogify-Project-Backend/src/routes"
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	router := mux.NewRouter()
+	router.Use(middleware.CORSMiddleware)
+	http.Handle("/", router)
+	routes.RegisterPostRoutes(router)
+	routes.RegisterUserRoutes(router)
+	log.Println("Listening on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", router))
+	
+}
