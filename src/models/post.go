30603883135456b7eead0a9342cd5e1cf5b37ab4
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-02 08:20:44 +0700

    feat:create Models: post, comment,reaction

---
+++ src/models/post.go
@@ -0,0 +1,30 @@
+package models
+
+import "time"
+
+type Post struct {
+	Id         int         `json:"id"`
+	User       VirtualUser `json:"user"`
+	Caption    string      `json:"caption"`
+	PostImages []PostImage `json:"postImages"`
+	CreateAt   time.Time   `json:"createAt"`
+	Comments   []Comment   `json:"comments"`
+}
+type PostImage struct {
+	Id       int    `json:"id"`
+	ImageURL string `json:"imageURL"`
+}
+type Comment struct {
+	Id            int         `json:"id"`
+	User          VirtualUser `json:"user"`
+	PostId        int         `json:"postId"`
+	Content       string      `json:"content"`
+	CreateAt      time.Time   `json:"creatAt"`
+	ParentComment *Comment    `json:"parentComment"`
+	Reaction      Reaction    `json:"reaction"`
+}
+type Reaction struct {
+	Id            int    `json:"id"`
+	UserS         []User `json:"users"`
+	CountReaction int    `json:"countReaction"`
+}
