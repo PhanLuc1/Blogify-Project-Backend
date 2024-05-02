49facd7e840330615c54dc89b1db78e1dad7cdd0
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-02 08:21:25 +0700

    feat: create Models: user and virtualuser

---
+++ src/models/user.go
@@ -0,0 +1,21 @@
+package models
+
+type User struct {
+	Id         int    `json:"id"`
+	Email      string `json:"email"`
+	Username   string `json:"username"`
+	Password   string `json:"password"`
+	State      bool   `json:"state"`
+	AvataImage string `json:"avataImage"`
+}
+type Follower struct {
+	Id         int
+	FollowerID int `json:"followerId"`
+	FolloweeID int `json:"folloeeID"`
+}
+type VirtualUser struct {
+	Id         int    `json:"id"`
+	Username   string `json:"username"`
+	State      bool   `json:"state"`
+	AvataImage string `json:"avataImage"`
+}
