03fd9ebd72559f64906dfba7ff9fdd9517203781
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-04 03:02:10 +0700

    feat: create function verifypassword

--- src/controller/usercontroller.go
+++ src/controller/usercontroller.go
@@ -13,3 +13,13 @@ func HashPassword(password string) string {
 	}
 	return string(bytes)
 }
+func VerifyPassword(userpassword string, givenpassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(givenpassword), []byte(userpassword))
+	valid := true
+	msg := ""
+	if err != nil {
+		msg = "Login Or Passowrd is Incorerct"
+		valid = false
+	}
+	return valid, msg
+}
