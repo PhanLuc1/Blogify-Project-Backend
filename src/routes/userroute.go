7ff49f7a9d556a9c8db3e70056efb81b3ffd3898
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-04 04:29:57 +0700

    refactor: insert /user/signup endpoint into registeruseroutes

--- src/routes/userroute.go
+++ src/routes/userroute.go
@@ -1,6 +1,10 @@
 package routes
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/PhanLuc1/Blogify-Project-Backend/src/controller"
+	"github.com/gorilla/mux"
+)
 
 var RegisterUserRoutes = func(router *mux.Router) {
+	router.HandleFunc("/user/signup",controller.Signup).Methods("POST")
 }
