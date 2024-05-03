5cec750e8d947ae337259012e2868ca29a0dfb83
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-04 04:30:43 +0700

    feat: create function handle signup

--- src/controller/usercontroller.go
+++ src/controller/usercontroller.go
@@ -1,8 +1,13 @@
 package controller
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
+	"regexp"
 
+	"github.com/PhanLuc1/Blogify-Project-Backend/src/database"
+	"github.com/PhanLuc1/Blogify-Project-Backend/src/models"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -22,4 +27,34 @@ func VerifyPassword(userpassword string, givenpassword string) (bool, string) {
 		valid = false
 	}
 	return valid, msg
+}
+func Signup(w http.ResponseWriter, r *http.Request) {
+	var user models.User
+	err := json.NewDecoder(r.Body).Decode(&user)
+	if err != nil {
+		http.Error(w, err.Error(), 400)
+		return
+	}
+
+	emailRegex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	re := regexp.MustCompile(emailRegex)
+	if !re.MatchString(*&user.Email) {
+		w.WriteHeader(422)
+		w.Write([]byte(`{"error": "Invalid email format"}`))
+		return
+	}
+
+	password := HashPassword(user.Password)
+	user.Password = password
+	query := "INSERT INTO user (email, username, password, avataImage) VALUES (?, ?, ?, ?)"
+	_, err = database.Client.Query(query, user.Email, user.Username, user.Password, user.AvataImage)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	w.WriteHeader(201)
+	
+	w.Write([]byte(`{"message": "Your account has been created"}`))
+
 }
