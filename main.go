16a24587bcd95cb964a1e0ef7ddaa616cbcd22bc
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-04 04:27:45 +0700

    rm:

--- main.go
+++ main.go
@@ -12,10 +12,8 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.Use(middleware.CORSMiddleware)
-	http.Handle("/", router)
 	routes.RegisterPostRoutes(router)
 	routes.RegisterUserRoutes(router)
 	log.Println("Listening on port 8000")
 	log.Fatal(http.ListenAndServe(":8000", router))
-	
 }
