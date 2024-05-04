85db433bee319e16337b5c394594737b7dc435a2
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-04 18:48:07 +0700

    feat:create handlefunc getallpost

---
+++ src/controller/postcontroller.go
@@ -0,0 +1,37 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/PhanLuc1/Blogify-Project-Backend/src/database"
+	"github.com/PhanLuc1/Blogify-Project-Backend/src/models"
+)
+
+func GetAllPost(w http.ResponseWriter, r *http.Request) {
+	var posts []models.Post
+	var userId int
+	query := "SELECT * FROM post"
+	result, err := database.Client.Query(query)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	for result.Next() {
+		var post models.Post
+		result.Scan(&post.Id, userId, &post.Caption, &post.CreateAt)
+		post.PostImages, err = database.GetImageProduct(post.Id)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		post.User, err = database.GetInfoUser(userId)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		posts = append(posts, post)
+	}
+	w.WriteHeader(200)
+	json.NewEncoder(w).Encode(posts)
+}
