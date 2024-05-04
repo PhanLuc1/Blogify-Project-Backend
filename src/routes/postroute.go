2a5445c590aa11c31ffa50014a7bd2d9ed1f7e4b
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-04 18:48:54 +0700

    refactor: add endpoint get all post

--- src/routes/postroute.go
+++ src/routes/postroute.go
@@ -1,7 +1,10 @@
 package routes
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/PhanLuc1/Blogify-Project-Backend/src/controller"
+	"github.com/gorilla/mux"
+)
 
 var RegisterPostRoutes = func(router *mux.Router) {
-
+	router.HandleFunc("/post", controller.GetAllPost).Methods("GET")
 }
