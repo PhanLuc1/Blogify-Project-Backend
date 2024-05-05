293b293532b1cd4db02f0e8ae506b94568893b43
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-05 16:04:49 +0700

    rm: delete models reaction and create function get postimage

--- src/models/post.go
+++ src/models/post.go
@@ -1,6 +1,10 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/PhanLuc1/Blogify-Project-Backend/src/database"
+)
 
 type Post struct {
 	Id         int         `json:"id"`
@@ -9,6 +13,7 @@ type Post struct {
 	PostImages []PostImage `json:"postImages"`
 	CreateAt   time.Time   `json:"createAt"`
 	Comments   []Comment   `json:"comments"`
+	Reaction   Reaction    `json:"reaction"`
 }
 type PostImage struct {
 	Id          int    `json:"id"`
@@ -24,8 +29,18 @@ type Comment struct {
 	ParentComment *Comment  `json:"parentComment"`
 	Reaction      Reaction  `json:"reaction"`
 }
-type Reaction struct {
-	Id            int    `json:"id"`
-	UserS         []User `json:"users"`
-	CountReaction int    `json:"countReaction"`
+func GetImageProduct(postId int) (postImages []PostImage, err error) {
+	result1, err := database.Client.Query("SELECT postimage.id, postimage.imageURL, postimage.description FROM postimage WHERE postId = ?", postId)
+	if err != nil {
+		return nil, err
+	}
+	for result1.Next() {
+		var imageTemp PostImage
+		err := result1.Scan(&imageTemp.Id, &imageTemp.ImageURL, &imageTemp.Description)
+		if err != nil {
+			return nil, err
+		}
+		postImages = append(postImages, imageTemp)
+	}
+	return postImages, nil
 }
