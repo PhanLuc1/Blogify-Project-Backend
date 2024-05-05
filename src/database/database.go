ec57b14cd0a99408920c85480057b6e88bcc51d0
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-05 16:05:37 +0700

    rm: delete function get imange product ang function get user information

--- src/database/database.go
+++ src/database/database.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/PhanLuc1/Blogify-Project-Backend/src/models"
 	_ "github.com/go-sql-driver/mysql"
 )
 
@@ -23,32 +22,3 @@ func DBSet() *sql.DB {
 }
 
 var Client *sql.DB = DBSet()
-
-func GetImageProduct(postId int) (postImages []models.PostImage, err error) {
-	result1, err := Client.Query("SELECT postimage.id, postimage.imageURL, postimage.description FROM postimage WHERE postId = ?", postId)
-	if err != nil {
-		return nil, err
-	}
-	for result1.Next() {
-		var imageTemp models.PostImage
-		err := result1.Scan(&imageTemp.Id, &imageTemp.ImageURL, &imageTemp.Description)
-		if err != nil {
-			return nil, err
-		}
-		postImages = append(postImages, imageTemp)
-	}
-	return postImages, nil
-}
-func GetInfoUser(userId int) (user models.User, err error) {
-	err = Client.QueryRow("SELECT user.id, user.email, user.username, user.state, user.avataImage FROM user").Scan(
-		&user.Id,
-		&user.Email,
-		&user.Username,
-		&user.State,
-		&user.AvataImage,
-	)
-	if err != nil {
-		return user, err
-	}
-	return user, nil
-}
