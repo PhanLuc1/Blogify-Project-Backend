ccdf2423341c2974143fdd8f8ee2e4bb3e299ffc
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-20 17:53:28 +0700

    recfactor: add and fix endpoint form params to path, add endpoint

--- src/routes/postroute.go
+++ src/routes/postroute.go
@@ -8,8 +8,8 @@ import (
 var RegisterPostRoutes = func(router *mux.Router) {
 	router.HandleFunc("/posts", controller.GetAllPost).Methods("GET")
 	router.HandleFunc("/posts/creating", controller.CreateNewPost).Methods("POST")
-	//router.HandleFunc("/posts/{postid}", controller.GetPostById).Methods("GET")
+	router.HandleFunc("/posts/{postid}", controller.GetPostById).Methods("GET")
 	router.HandleFunc("/posts/{postid}/comment", controller.CreateComment).Methods("POST")
 	router.HandleFunc("/posts/{postid}/reaction", controller.PostReact).Methods("GET")
-	router.HandleFunc("/posts/{postId}/comment/reaction", controller.CommentReact).Methods("GET")
+	router.HandleFunc("/posts/{commentid}/reaction", controller.CommentReact).Methods("GET")
 }
