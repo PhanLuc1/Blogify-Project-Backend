3f8ac22a49f567a7a675b84437a65f9df3b3384f
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-20 17:50:48 +0700

    refactor: change endpoint from params to path

--- src/controller/commentcontroller.go
+++ src/controller/commentcontroller.go
@@ -8,11 +8,14 @@ import (
 	"github.com/PhanLuc1/Blogify-Project-Backend/src/auth"
 	"github.com/PhanLuc1/Blogify-Project-Backend/src/database"
 	"github.com/PhanLuc1/Blogify-Project-Backend/src/models"
+	"github.com/gorilla/mux"
 )
 
 func CreateComment(w http.ResponseWriter, r *http.Request) {
 	var comment models.Comment
 	var query string
+	vars := mux.Vars(r)
+	postId := vars["postid"]
 	claims, err := auth.GetUserFromToken(r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusUnauthorized)
@@ -26,7 +29,7 @@ func CreateComment(w http.ResponseWriter, r *http.Request) {
 	createAt := time.Now().Format("2006-01-02 15:04:05")
 	if comment.ParentCommentID.Int64 != 0 {
 		query = "INSERT INTO comment (userId, postId, parentCommentId, content, createAt) VALUES (?, ?, ?, ?, ?)"
-		_, err = database.Client.Query(query, claims.UserId, comment.PostId, comment.ParentCommentID.Int64, comment.Content, createAt)
+		_, err = database.Client.Query(query, claims.UserId, postId, comment.ParentCommentID.Int64, comment.Content, createAt)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -41,13 +44,14 @@ func CreateComment(w http.ResponseWriter, r *http.Request) {
 	}
 }
 func CommentReact(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	commentId := vars["commentid"]
 	claims, err := auth.GetUserFromToken(r)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusUnauthorized)
 		return
 	}
-	commentId := r.URL.Query().Get("commentId")
-	query := "INSERT INTO comment_reaction (userId, postId) VALUES (?, ?)"
+	query := "INSERT INTO comment_reaction (userId, comentId) VALUES (?, ?)"
 	_, err = database.Client.Query(query, claims.UserId, commentId)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
