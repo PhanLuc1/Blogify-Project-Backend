559a74373359d65b29fed5802ddd946046bc889d
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-27 15:54:47 +0700

    rm: delete function get newtoken by refreshtoken

--- src/auth/authenticate.go
+++ src/auth/authenticate.go
@@ -1,12 +1,10 @@
 package auth
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
 
 	generate "github.com/PhanLuc1/Blogify-Project-Backend/src/middleware"
-	"github.com/PhanLuc1/Blogify-Project-Backend/src/models"
 )
 
 func GetUserFromToken(r *http.Request) (*generate.SignedDetails, error) {
@@ -19,20 +17,4 @@ func GetUserFromToken(r *http.Request) (*generate.SignedDetails, error) {
 		return claims, nil
 	}
 	return nil, fmt.Errorf("token not provided")
-}
-func GetNewTokenFromRefreshToken(w http.ResponseWriter, r *http.Request) {
-	refreshtToken := r.Header.Get("RefreshToken")
-	if refreshtToken != "" {
-		claims, _ := generate.ValidateToken(refreshtToken)
-		if claims == nil {
-			w.WriteHeader(401)
-			return
-		}
-		var tokenUser models.Token
-		token, refreshToken, _ := generate.TokenGeneration(claims.UserId)
-		tokenUser.Token = token
-		tokenUser.Refreshtoken = refreshToken
-		w.WriteHeader(200)
-		json.NewEncoder(w).Encode(token)
-	}
 }
