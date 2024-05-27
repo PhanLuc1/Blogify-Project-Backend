04a41e27146c22b093b16ebc551ce447a9f3b45a
PhanLuc1 <lucptt.22it@vku.udn.vn>
2024-05-28 02:27:57 +0700

    feat: add middleware appilication/octet-stream

--- src/middleware/middleware.go
+++ src/middleware/middleware.go
@@ -12,7 +12,7 @@ func CORSMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE, PATCH")
 		w.Header().Set("Access-Control-Allow-Headers", "ngrok-skip-browser-warning,Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, X-Max, token, refreshToken")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-
+		w.Header().Set("Content-Type", "application/octet-stream")
 		if r.Method == "OPTIONS" {
 			w.WriteHeader(http.StatusOK)
 			return
